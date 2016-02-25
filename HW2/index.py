8f950a21713f93f9fa6b1f182f4912e35c8a0e60
Akshat Dubey <akshatdubey@outlook.com>
2016-02-26 00:38:54 +0800

    ignored reuters data and setup file io for both search and index

--- HW2/index.py
+++ HW2/index.py
@@ -1,15 +1,15 @@
-#!/usr/bin/python
-import re
+#HW2 by akshat dubey
+
 import nltk
 import sys
 import getopt
 
-            	
+           	
 
 def usage():
     print "usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file"
 
-input_file_b = input_file_t = output_file = None
+documents_dir_i = dictionary_file_d = postings_file_p = None
 try:
     opts, args = getopt.getopt(sys.argv[1:], 'i:d:p:')
 except getopt.GetoptError, err:
@@ -21,15 +21,17 @@ for o, a in opts:
     elif o == '-d':
         dictionary_file_d = a
     elif o == '-p':
-        postings_file = a
+        postings_file_p = a
     else:
         assert False, "unhandled option"
-if documents_dir_i == None or dictionary_file_d == None or postings_file == None:
+if documents_dir_i == None or dictionary_file_d == None or postings_file_p == None:
     usage()
     sys.exit(2)
 
 # t0 = time.time()
-# LM = build_LM(input_file_b)
+
+
+# LM = build_index(input_file_b)
 # test_LM(input_file_t, output_file, LM)
 # t1 = time.time()
 
---
+++ HW2/search.py
@@ -0,0 +1,38 @@
+#HW2 by akshat dubey
+
+import nltk
+import sys
+import getopt
+
+           	
+
+def usage():
+    print "usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results"
+
+dictionary_file_d = postings_file_p = query_file_q = output_file_o = None
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'd:p:q:o:')
+except getopt.GetoptError, err:
+    usage()
+    sys.exit(2)
+for o, a in opts:
+    if o == '-d':
+        dictionary_file_d = a
+    elif o == '-p':
+        postings_file_p = a
+    elif o == '-q':
+        query_file_q = a
+    elif o == '-o':
+        output_file_o = a
+    else:
+        assert False, "unhandled option"
+if dictionary_file_d == None or postings_file_p == None or query_file_q == None or output_file_o == None:
+    usage()
+    sys.exit(2)
+
+# t0 = time.time()
+# LM = build_LM(input_file_b)
+# test_LM(input_file_t, output_file, LM)
+# t1 = time.time()
+
+# print "this run with n=" + str(gram_size) + " took " + str(t1-t0)
