667d6cb9bc9515fbc416f5b70d97c5701f5d3aaf
Varun Patro <varun.kumar.patro@gmail.com>
2016-03-03 23:03:44 +0800

    implement shunting-yard for boolean expressions

--- HW2/search.py
+++ HW2/search.py
@@ -4,6 +4,50 @@ import nltk
 import sys
 import getopt
 
+def parse_query(query):
+    args = []
+    ops = []
+    tokens = nltk.word_tokenize(query)
+    for token in tokens:
+        print token
+        if (token == 'NOT'):
+            args.append(token)
+        elif (token == 'AND'):
+            if (len(args) > 0):
+                while(args[len(args) - 1] == 'NOT'):
+                    ops.append(args.pop)
+            args.append(token)
+        elif (token == 'OR'):
+            if (len(args) > 0):
+                while(args[len(args) - 1] == 'NOT' or args[len(args) - 1] == 'AND'):
+                    ops.append(args.pop)
+            args.append(token)
+        elif (token == '('):
+            args.append(token)
+        elif (token == ')'):
+            while (len(args) > 0):
+                arg = args.pop()
+                if (arg != '('):
+                    ops.append(arg)
+                else:
+                    break
+            if (len(args) == 0):
+                print "Query is invalid. Brackets don't match."
+                sys.exit(1)
+        else:
+            ops.append(token)
+
+    while(len(args) > 0):
+        ops.append(args.pop())
+
+    return ops
+
+def answer_queries():
+    queries = open(query_file_q, 'r')
+    for query in queries:
+        print query
+        ops = parse_query(query)
+
 def usage():
     print "usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results"
 
@@ -27,6 +71,10 @@ for o, a in opts:
 if dictionary_file_d == None or postings_file_p == None or query_file_q == None or output_file_o == None:
     usage()
     sys.exit(2)
+
+
+print ("answering queries...")
+answer_queries()
 
 # t0 = time.time()
 # LM = build_LM(input_file_b)
