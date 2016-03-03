13b1fcbed2a55cc4c0aaa4851d11ba24abbba772
Varun Patro <varun.kumar.patro@gmail.com>
2016-03-04 07:08:33 +0800

    optimize boolean operation execution

--- HW2/search.py
+++ HW2/search.py
@@ -5,7 +5,7 @@ import nltk
 import sys
 import getopt
 import cPickle as pickle
-
+count = 0
 stemmer = nltk.stem.porter.PorterStemmer()
 all_list = []
 
@@ -14,18 +14,17 @@ def parse_query(query):
     ops = []
     tokens = nltk.word_tokenize(query)
     for token in tokens:
-        print token
         if (token == 'NOT'):
             args.append(token)
         elif (token == 'AND'):
             if (len(args) > 0):
                 while(args[len(args) - 1] == 'NOT'):
-                    ops.append(args.pop)
+                    ops.append(args.pop())
             args.append(token)
         elif (token == 'OR'):
             if (len(args) > 0):
                 while(args[len(args) - 1] == 'NOT' or args[len(args) - 1] == 'AND'):
-                    ops.append(args.pop)
+                    ops.append(args.pop())
             args.append(token)
         elif (token == '('):
             args.append(token)
@@ -49,17 +48,19 @@ def parse_query(query):
     return ops
 
 def get_postings_list(word):
-    offset = words[word][1] 
+    offset = words[word][1]
     postings.seek(offset)
     line = postings.readline()
     postings_list = ast.literal_eval(line)
     return postings_list
 
 def not_list(xs):
+    global count
     zs = []
     ys = all_list
     x_i, y_i = 0, 0
     while(x_i < len(xs)):
+        count += 1
         x, y = xs[x_i], ys[y_i]
         if (type(x) == type((1, 2))):
             x = x[0]
@@ -72,14 +73,15 @@ def not_list(xs):
         else:
             zs.append(y)
             y_i += 1
-    
     zs += ys[y_i:]
     return zs
 
 def and_list(xs, ys):
+    global count
     zs = []
     x_i, y_i = 0, 0
     while(x_i < len(xs) and y_i < len(ys)):
+        count += 1
         x, y = xs[x_i], ys[y_i]
         if (type(x) == type((1, 2))):
             x = x[0]
@@ -98,9 +100,11 @@ def and_list(xs, ys):
     return zs
 
 def or_list(xs, ys):
+    global count
     zs = []
     x_i, y_i = 0, 0
     while(x_i < len(xs) and y_i < len(ys)):
+        count += 1
         x, y = xs[x_i], ys[y_i]
         if (type(x) == type((1, 2))):
             x = x[0]
@@ -127,15 +131,48 @@ def or_list(xs, ys):
 
 def perform_operations(ops):
     os = []
-    for op in ops:
+    for op_i in range(len(ops)):
+        op = ops[op_i]
         if (op == 'NOT'):
             idxs = os.pop()
             os.append(not_list(idxs))
         elif (op == 'AND'):
+            # reorder operand stack os for maximum efficiency
+            and_count = 1
+            for i in range(op_i + 1, len(ops)):
+                if (ops[i] == 'AND'):
+                    and_count += 1
+                else:
+                    break
+
+            # need to reorder and_count + 1 top elements in the os
+            if (and_count > 1):
+                os_vals = os[:and_count + 1]
+                os_vals.sort(key=len)
+                os = os[:and_count + 1]
+                os_vals.reverse()
+                os += os_vals
+
             idxs1 = os.pop()
             idxs2 = os.pop()
             os.append(and_list(idxs1, idxs2))
         elif (op == 'OR'):
+            # reorder operand stack os for maximum efficiency
+            or_count = 1
+            for i in range(op_i + 1, len(ops)):
+                if (ops[i] == 'OR'):
+                    or_count += 1
+                else:
+                    break
+
+            # need to reorder or_count + 1 top elements in the os
+            if (or_count > 1):
+                os_vals = os[:or_count + 1]
+                os_vals.sort(key=len)
+                os = os[:or_count + 1]
+                os_vals.reverse()
+                os += os_vals
+
             idxs1 = os.pop()
             idxs2 = os.pop()
             os.append(or_list(idxs1, idxs2))
@@ -145,11 +182,14 @@ def perform_operations(ops):
     return os.pop()
 
 def answer_queries():
-    queries = open(query_file_q, 'r')
+    global count
+    queries_file = open(query_file_q, 'r')
+    queries = queries_file.read().splitlines()
     output = open(output_file_o, 'w')
     for query in queries:
-        print query
+        count = 0
         ops = parse_query(query)
+        print query
         print ops
         output_list = perform_operations(ops)
         notFirst = False
@@ -161,6 +201,7 @@ def answer_queries():
             output.write(str(id))
             notFirst = True
         output.write('\n')
+        print "NUM COUNT = " + str(count)
 
 def usage():
     print "usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results"
@@ -183,8 +224,12 @@ for o, a in opts:
     else:
         assert False, "unhandled option"
 if dictionary_file_d == None or postings_file_p == None or query_file_q == None or output_file_o == None:
-    usage()
-    sys.exit(2)
+    dictionary_file_d = 'dictionary.txt'
+    postings_file_p = 'postings.txt'
+    query_file_q = 'queries.txt'
+    output_file_o = 'output.txt'
+    # usage()
+    # sys.exit(2)
 
 print "reading dictionary"
 words = pickle.load(open(dictionary_file_d, 'r'))
