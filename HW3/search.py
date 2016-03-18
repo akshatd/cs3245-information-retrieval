2ab14da2e0c3f0aeb2efc7187af2706cd7f7ad96
Varun Patro <varun.kumar.patro@gmail.com>
2016-03-18 15:10:13 +0800

    Document search.py

--- HW3/search.py
+++ HW3/search.py
@@ -8,29 +8,40 @@ import nltk
 import sys
 import time
 
+DEBUG_MODE = False
 
 stemmer = nltk.stem.porter.PorterStemmer()
 all_list = []
 all_dict = dict()
 N = 1
-Scores = dict()
 
 def parse_query(query):
+    '''
+    Stems the lower case query using Porter's stemmer.
+    :param query:
+    :return: stemmed query
+    '''
     return stemmer.stem(query.lower())
 
-def ltc_vec(query):
-    global Scores
+def cosine_score(query):
+    '''
+    Scores the documents using a cosine score based on the lnc.ltc ranking scheme.
+    Does so by first, computing the query vector using ltc scheme. Then for each word in the query,
+    the score of the document containg that word is updated. Finally, the score of each document is normalized,
+    and returned.
+    :param query:
+    :return Scores:
+    '''
     Scores = dict()
     q_terms = nltk.word_tokenize(query)
     q_vec = dict()
     total_wtq = 0
 
     for term in q_terms:
-        raw_tf = q_terms.count(term)
-        tf = 1 + math.log(raw_tf, 10)
+        tf = 1 + math.log(q_terms.count(term), 10)
         if term in words:
             postings_info = words[term]
-            df = (postings_info[0])
+            df = postings_info[0]
             idf = math.log(N / df, 10)
             wtq = tf * idf
             total_wtq += wtq ** 2
@@ -56,16 +67,24 @@ def ltc_vec(query):
     for tid in Scores:
         Scores[tid] = Scores[tid] / all_dict[tid]
 
+    return Scores
+
 def answer_queries():
+    '''
+    Reads the queries from the file in order and for each query, writes the at most 10 docIds to the output file.
+    :return None:
+    '''
     queries_file = open(query_file_q, 'r')
     queries = queries_file.read().splitlines()
     output = open(output_file_o, 'w')
     for raw_query in queries:
         query = parse_query(raw_query)
-        q_vec = ltc_vec(query)
+        Scores = cosine_score(query)
         sorted_scores = sorted(Scores.items(), key=lambda x: (x[1], x[0]), reverse=True)
+        output_list = []
         for i in range(min(len(sorted_scores), 10)):
-            output.write(str(sorted_scores[i][0]) + ' ')
+            output_list.append(sorted_scores[i][0])
+        output.write(' '.join([str(i) for i in output_list]))
         output.write('\n')
 
 def usage():
@@ -89,12 +108,14 @@ for o, a in opts:
     else:
         assert False, "unhandled option"
 if dictionary_file_d == None or postings_file_p == None or query_file_q == None or output_file_o == None:
-    dictionary_file_d = 'dictionary.txt'
-    postings_file_p = 'postings.txt'
-    query_file_q = 'queries.txt'
-    output_file_o = 'output.txt'
-    # usage()
-    # sys.exit(2)
+    if DEBUG_MODE:
+        dictionary_file_d = 'dictionary.txt'
+        postings_file_p = 'postings.txt'
+        query_file_q = 'queries.txt'
+        output_file_o = 'output.txt'
+    else:
+        usage()
+        sys.exit(2)
 
 t0 = time.time()
 print "reading dictionary"
